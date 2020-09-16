11a53923ce8d575aa035d786a1d00b99410afe09
Aldi Andika <aldi.andika@gmail.com>
2020-09-17 00:14:11 +0700

    lib/pages/cart_page.dart : Tambah panel bawah di bagian Cart Page

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -23,22 +23,71 @@ class _CartPageState extends State<CartPage> {
             },
           ),
         ),
-        body: SingleChildScrollView(
-          child: Container(
-            padding: EdgeInsets.symmetric(
-              horizontal: 8.0,
-              vertical: 12.0,
+        body: Column(
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                child: Container(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 8.0,
+                    vertical: 12.0,
+                  ),
+                  child: Column(
+                    children: [
+                      // Cart Telemedicine
+                      CartTelemedicine(),
+                      // End of Cart Telemedicine
+                      SizedBox(
+                        height: 24.0,
+                      ),
+                      CartProduct(),
+                    ],
+                  ),
+                ),
+              ),
             ),
-            child: Column(
-              children: [
-                // Cart Telemedicine
-                CartTelemedicine(),
-                // End of Cart Telemedicine
-                SizedBox(height:24.0,),
-                CartProduct(),
-              ],
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Card(
+                elevation: 10.0,
+                child: Container(
+                  height: MediaQuery.of(context).size.height * 0.1,
+                  width: MediaQuery.of(context).size.width,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Text(
+                        'Total: Rp 270.000,00',
+                        style: TextStyle(
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        overflow: TextOverflow.clip,
+                      ),
+                      Container(
+                        width: 100.0,
+                        height: 26.0,
+                        child: RaisedButton(
+                          child: Text(
+                            'Checkout',
+                            style: TextStyle(fontSize: 12.0),
+                          ),
+                          color: Color(0xFF0099FF),
+                          textColor: Colors.white,
+                          elevation: 3.0,
+                          shape: RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(8.0)),
+                          ),
+                          onPressed: () => print('Checkout produk'),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ),
-          ),
+          ],
         ),
       ),
     );
