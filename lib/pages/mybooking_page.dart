6501681ab1e758aa5c4d92a28a8c677f46d6f6fe
Aldi Andika <aldi.andika@gmail.com>
2020-09-27 15:56:30 +0700

    lib/pages/mybooking_page.dart : merapikan tampilan chat dari telemedicine

--- lib/pages/mybooking_page.dart
+++ lib/pages/mybooking_page.dart
@@ -17,96 +17,94 @@ class _MybookingPageState extends State<MybookingPage> {
   Widget build(BuildContext context) {
     double heightScreen = Get.height;
     double widthScreen = Get.width;
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Center(child: Text("My Booking")),
-        ),
-        body: Container(
-          height: heightScreen,
-          width: widthScreen,
-          color: Colors.white,
-          padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-          child: Column(
-            children: [
-              Container(
-                // padding: EdgeInsets.all(10),
-                child: Row(
-                  children: [
-                    Container(
-                      height: 30,
-                      decoration: BoxDecoration(
-                        color: index == 0 ? Colors.blue : Colors.white,
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                      child: MaterialButton(
-                        child: Text(
-                          "Dermatologist",
-                          style: TextStyle(
-                            color: index == 0 ? Colors.white : Colors.blue,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 15,
-                          ),
-                        ),
-                        onPressed: () {
-                          setState(() {
-                            index = 0;
-                          });
-                        },
-                      ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text("My Booking")),
+      ),
+      body: Container(
+        height: heightScreen,
+        width: widthScreen,
+        color: Colors.white,
+        padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+        child: Column(
+          children: [
+            Container(
+              // padding: EdgeInsets.all(10),
+              child: Row(
+                children: [
+                  Container(
+                    height: 30,
+                    decoration: BoxDecoration(
+                      color: index == 0 ? Colors.blue : Colors.white,
+                      borderRadius: BorderRadius.circular(30),
                     ),
-                    Container(
-                      height: 30,
-                      decoration: BoxDecoration(
-                        color: index == 1 ? Colors.blue : Colors.white,
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                      child: MaterialButton(
-                        child: Text(
-                          "Telemedicine",
-                          style: TextStyle(
-                            color: index == 1 ? Colors.white : Colors.blue,
-                            fontWeight: FontWeight.bold,
-                          ),
+                    child: MaterialButton(
+                      child: Text(
+                        "Dermatologist",
+                        style: TextStyle(
+                          color: index == 0 ? Colors.white : Colors.blue,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15,
                         ),
-                        onPressed: () {
-                          setState(() {
-                            index = 1;
-                          });
-                        },
                       ),
+                      onPressed: () {
+                        setState(() {
+                          index = 0;
+                        });
+                      },
                     ),
-                    Container(
-                      height: 30,
-                      decoration: BoxDecoration(
-                        color: index == 2 ? Colors.blue : Colors.white,
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                      child: MaterialButton(
-                        child: Text(
-                          "Care",
-                          style: TextStyle(
-                            color: index == 2 ? Colors.white : Colors.blue,
-                            fontWeight: FontWeight.bold,
-                          ),
+                  ),
+                  Container(
+                    height: 30,
+                    decoration: BoxDecoration(
+                      color: index == 1 ? Colors.blue : Colors.white,
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: MaterialButton(
+                      child: Text(
+                        "Telemedicine",
+                        style: TextStyle(
+                          color: index == 1 ? Colors.white : Colors.blue,
+                          fontWeight: FontWeight.bold,
                         ),
-                        onPressed: () {
-                          setState(() {
-                            index = 2;
-                          });
-                        },
                       ),
+                      onPressed: () {
+                        setState(() {
+                          index = 1;
+                        });
+                      },
                     ),
-                  ],
-                ),
+                  ),
+                  Container(
+                    height: 30,
+                    decoration: BoxDecoration(
+                      color: index == 2 ? Colors.blue : Colors.white,
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: MaterialButton(
+                      child: Text(
+                        "Care",
+                        style: TextStyle(
+                          color: index == 2 ? Colors.white : Colors.blue,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          index = 2;
+                        });
+                      },
+                    ),
+                  ),
+                ],
               ),
-              index == 0
-                  ? Expanded(child: MyBookingDermatologist())
-                  : index == 1
-                      ? Expanded(child: MyBookingTelemedicine())
-                      : Expanded(child: MyBookingCare()),
-            ],
-          ),
+            ),
+            index == 0
+                ? Expanded(child: MyBookingDermatologist())
+                : index == 1
+                ? Expanded(child: MyBookingTelemedicine())
+                : Expanded(child: MyBookingCare()),
+          ],
         ),
       ),
     );
