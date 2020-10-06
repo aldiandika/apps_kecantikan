94f7e2dacd02b0f98ad3070b34bfc8a68c8bd996
Aldi Andika <aldi.andika@gmail.com>
2020-10-06 13:07:26 +0700

    lib/widgets/subwidget_card_rec_treat.dart : Mengubah decoration card

--- lib/widgets/subwidget_card_rec_treat.dart
+++ lib/widgets/subwidget_card_rec_treat.dart
@@ -15,17 +15,16 @@ class _CardRecTreatState extends State<CardRecTreat> {
         horizontal: 6.0,
       ),
       shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(
-          Radius.circular(8.0),
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(28.0),
+          bottomRight: Radius.circular(28.0),
         ),
       ),
+      color: Color(0xFFFFEBE5),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Container(
-            height: 160.0,
-            width: 180.0,
-            color: Colors.blueGrey,
+          ClipRRect(
+            borderRadius: BorderRadius.only(topLeft: Radius.circular(28.0),),
             child: Image.asset(
               'assets/images/facial.jpg',
               fit: BoxFit.cover,
@@ -33,45 +32,50 @@ class _CardRecTreatState extends State<CardRecTreat> {
           ),
 
           Container(
-            padding: EdgeInsets.all(2.0),
-            margin: EdgeInsets.all(2.0),
-            child: Text(
-              'Peeling Scrub',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 9.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-              ),
-              overflow: TextOverflow.ellipsis,
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.all(2.0),
-            margin: EdgeInsets.all(2.0),
-            child: Text(
-              'Rp 130.000,00 - Rp 250.000,00',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 9.0,
-                fontWeight: FontWeight.bold,
-                color: Color(0xFFFF7583),
-              ),
-              overflow: TextOverflow.ellipsis,
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.all(2.0),
-            margin: EdgeInsets.all(4.0),
-            child: Text(
-              'Klinik Jaya Sentosa Bandung',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 9.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.black45,
-              ),
-              overflow: TextOverflow.clip,
+            margin: EdgeInsets.symmetric(vertical: 6.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  margin: EdgeInsets.all(2.0),
+                  child: Text(
+                    'Peeling Scrub',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 13.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(2.0),
+                  child: Text(
+                    'Rp 130.000,00 - Rp 250.000,00',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 9.0,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFFFF7583),
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(2.0),
+                  child: Text(
+                    'Klinik Jaya Sentosa Bandung',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 9.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black45,
+                    ),
+                    overflow: TextOverflow.clip,
+                  ),
+                ),
+              ],
             ),
           ),
         ],
